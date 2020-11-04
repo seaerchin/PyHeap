fa98422835a2ee23f4f3b5d5a4962e8d671e87fb
seaerchin <jer005@e.ntu.edu.sg>
2020-11-04 21:31:57 +0800

    added example

--- heap.py
+++ heap.py
@@ -6,33 +6,37 @@ class Heap():
         ls is a list which is not assumed to be a heap; f is the comparison function which is used to construct the heap.
         of note, we assume the following invariants about the heap: left(i) returns the left child of heap rooted at i; right(i) returns the right child of heap rooted at i.
         """ 
-        self.ls = self.buildMaxHeap(ls)
+        self.ls = ls
         self.f = f
     
-    def maxHeapify(self, ls, i):
+    def maxHeapify(self, i):
         """ maxHeapify fixes the max heap at i. in particular, it assumes that left(i) and right(i) are both heaps but does not assume that ls[i] itself is a heap """ 
         l, r = self.left(i), self.right(i)
         # first, assume that the heap invariant holds 
         largest = i
-        if l <= len(ls) and self.f(ls[l], ls[i]):
+        if l < len(self.ls) and self.f(self.ls[l], self.ls[largest]):
             largest = l
-        if r <= len(ls) and self.f(ls[r], ls[i]):
+        if r < len(self.ls) and self.f(self.ls[r], self.ls[largest]):
             largest = r
         # invariant violated -> we need to fix this
         if largest != i: 
-            ls[largest], ls[i] = ls[i], ls[largest]
-            ls = self.maxHeapify(ls, largest)
-        return ls
+            self.ls[largest], self.ls[i] = self.ls[i], self.ls[largest]
+            self.maxHeapify(largest)
 
     def left(self, i): 
-        # NOTE: caller checks for indexError
         return 2 * i
     
     def right(self, i):
-        # NOTE: caller checks for indexError
         return 2 * i + 1
 
-    def buildMaxHeap(self, arr): 
-        for i in range(len(arr) // 2, 0, -1):
-            self.maxHeapify(arr, i)
-        return arr
+    def buildMaxHeap(self): 
+        for i in range(len(self.ls) // 2, -1, -1):
+            self.maxHeapify(i)
+
+    def getList(self):
+        return self.ls
+
+if __name__ == '__main__':
+    heap = Heap([3, 2, 5, 6, 10, 7, 1])
+    heap.buildMaxHeap()
+    print(heap.getList())
